c6dcb1afb6ada31cfdd1a805e6cadb95b8a5114c
Rystambek <rustambekjumaqulov@gmail.com>
2023-02-17 03:14:24 +0500

    init

--- telegram/message.py
+++ telegram/message.py
@@ -1,5 +1,9 @@
+from .photo import Photo
 class Message:
     def __init__(self,message:dict):
         self.message_id = message['message_id']
         self.from_user = message['from']
-        self.text = message['text']
+        if 'text' in message.keys():
+            self.text = message['text']
+        elif 'photo' in message.keys():
+            self.photo = Photo(message['photo'][-1])
